9c5231b7bec85c48569debc3c2e35b509c2d7afc
ThugXD <vvalterzandamela@gmail.com>
2024-07-10 01:58:15 +0200

    2. thugxdEvent-Laravel

---
+++ 2-hdcEvents-Laravel/resources/views/Thug.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+@section('title', 'Thug')
+@section('content')
+    <h1>Hello ThugXD, Your first Blade</h1>
+    <a href="/">Voltar Para Welcome</a></br>
+    <a href="/Apelido">ThugXD</a>
+@endsection
